6ada0bb25637e05b9516d9ab0f7582fea7a46329
Sang Dang <sangdase170287@fpt.edu.vn>
2023-11-29 12:02:18 +0700

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import routes from '@/routes'
 import { store } from '@/store'
 import React from 'react'
@@ -8,6 +9,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
+        <Analytics />
         <Provider store={store}>
             <RouterProvider router={routes} />
         </Provider>
